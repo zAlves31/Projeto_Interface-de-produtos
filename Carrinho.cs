173be906c9162559078556e77a15a8dd825b6c84
João Victor Alves Bautista <52546124870@info.local>
2023-05-11 10:58:10 -0300

    first commit

---
+++ Carrinho.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_produto_interface
+{
+    public class Carrinho : ICarrinho
+    {
+        public float valor {get; set;}
+
+    // Lista onde vamos manipular os objetos
+        List<Produto> carrinho = new List<Produto>();
+
+        public void Adicionar(Produto produto)
+        {
+            carrinho.Add(produto);
+        }
+
+        public void Listar()
+        {
+            if (carrinho != null)
+            {
+                foreach (var Produto in carrinho)
+                {
+                    Console.WriteLine($"Codigo: {Produto.Codigo} - Nome: {Produto.Nome} - Preco: {Produto.Preco:C}");
+                    
+                }
+            }
+        }
+
+        public void Atualizar(int codigo, Produto _novoProduto)
+        {
+            carrinho.Find(x => x.Codigo == codigo).Nome = _novoProduto.Nome;
+            carrinho.Find(x => x.Codigo == codigo).Preco = _novoProduto.Preco;
+        }
+
+
+        public void Remover(Produto produto)
+        {
+            carrinho.Remove(produto);
+        }
+    }
+}
---
+++ ICarrinho.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_produto_interface
+{
+    // interface para a classe Carrinho
+    public interface ICarrinho
+    {
+        // definir quais os metodos deverao ser implementados
+        // na classe que herdar desta interface
+
+        // CRUD : Create / Read / Update / Delete
+
+        // Create
+        void Adicionar(Produto produto);
+
+        // Read
+        void Listar();
+
+        // Update
+        void Atualizar(int codigo,Produto produto);
+
+        // Delete
+        void Remover(Produto produto);
+    }
+}
